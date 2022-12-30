324ad5fcb2fcc83f8ba40218be07d949ecd83d4d
Andrey <bursov@web.de>
2022-12-30 14:51:25 +0100

    Task eleven complete

--- task3110/command/ZipCreateCommand.java
+++ task3110/command/ZipCreateCommand.java
@@ -1,7 +1,23 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager = getZipFileManager();
+            ConsoleHelper.writeMessage("Enter file/folder path to archive:");
+            String path = ConsoleHelper.readString();
+            zipFileManager.createZip(Paths.get(path));
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
